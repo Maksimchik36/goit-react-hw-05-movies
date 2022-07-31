6cefbb978e6caf5f27bca4f40b88487e054c5059
Maksimchik36 <Peltek1985@gmail.com>
2022-07-31 23:00:06 +0300

    add api-key and components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from "./App.styled";
+
+const App = () => {
+  return (
+    <Container>
+      React homework template
+    </Container>
+  );
+};
+
+
+export default App;
---
+++ src/service/movie-service.js
@@ -0,0 +1,18 @@
+const API_KEY = '2af9cbb95e748f3536884b81d35f838b';
+
+// пример API-запроса  https://api.themoviedb.org/3/movie/550?api_key=2af9cbb95e748f3536884b81d35f838b
+
+//     https://developers.themoviedb.org/3/trending/get-trending
+// /trending/get-trending список самых популярных фильмов на сегодня для создания коллекции на главной странице.
+//     https://developers.themoviedb.org/3/search/search-movies
+// / search / search - movies поиск кинофильма по ключевому слову на странице фильмов.
+//     https://developers.themoviedb.org/3/movies/get-movie-details
+// /movies/get - movie - details запрос полной информации о фильме для страницы кинофильма.
+//     https://developers.themoviedb.org/3/movies/get-movie-credits
+// /movies/get - movie - credits запрос информации о актёрском составе для страницы кинофильма.
+//     https://developers.themoviedb.org/3/movies/get-movie-reviews
+// /movies/get-movie-reviews запрос обзоров для страницы кинофильма.
+
+
+
+
