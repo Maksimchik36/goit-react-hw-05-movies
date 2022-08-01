48bf05fffbc678e0ea9605b46a46c1319743202e
Maksimchik36 <Peltek1985@gmail.com>
2022-08-01 17:04:10 +0300

    add components and settings

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,26 @@
-import { Container } from "./App.styled";
+// import { Container } from "./App.styled";
+import { Routes, Route } from 'react-router-dom';
+import Layout from "components/Layout";
+import Home from "components/Home";
+import Movies from "components/Movies";
+import MovieDetails from "components/MovieDetails";
+import Cast from "components/Cast";
+import Reviews from "components/Reviews";
 
 const App = () => {
   return (
-    <Container>
-      React homework template
-    </Container>
+    
+      <Routes>
+        <Route path="/" element={<Layout />} >
+          <Route path='/' element={<Home />} />
+          <Route path ='/movies' element={<Movies />} />
+        </Route>
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path ="movies/:movieId/cast" element={<Cast />} />
+          <Route path ="/movies/:movieId/reviews" element={<Reviews />} />
+        </Route>      
+      </Routes>
+   
   );
 };
 
